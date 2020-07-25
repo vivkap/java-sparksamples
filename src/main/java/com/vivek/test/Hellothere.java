05b58bb3ec9ac16e2e3a9a41811e3aff052bc322
Vivek.Kapoor <vivkap07@gmail.com>
2020-07-25 15:55:17 -0400

    Add Hello there test file

---
+++ src/main/java/com/vivek/test/Hellothere.java
@@ -0,0 +1,10 @@
+package com.vivek.test;
+
+public class Hellothere {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello There Elastic BeanStalk");
+    }
+
+}
